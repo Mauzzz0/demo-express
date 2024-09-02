001aa43b97e798daed8d0bc05241a2ff9107126b
Ruslan Semak <ruslan.semack1501@gmail.com>
2024-09-02 20:31:44 +0300

    cli: Исправлен generate

--- src/cli/generate.ts
+++ src/cli/generate.ts
@@ -1,11 +1,11 @@
+import 'reflect-metadata';
+
 import { validate } from '../validation/validate';
 import { GenerateCommandOptions } from './dto';
 import { words } from './mock.values';
 import { getRandomItems } from './utils';
 
-const params = {
-  count: process.argv[2],
-};
+const params = { count: process.argv[2] };
 const { count } = validate(GenerateCommandOptions, params);
 
 for (let i = 0; i < count; i++) {
