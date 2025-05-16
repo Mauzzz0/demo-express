ba927fe13f7b52d08d2d6aee6131fefdc7894e24
Ruslan Semak <ruslan.semack1501@gmail.com>
2025-05-16 11:48:33 +0300

    feat: PinoLogger

--- src/logger/pino.logger.ts
+++ src/logger/pino.logger.ts
@@ -4,6 +4,8 @@ import pretty from 'pino-pretty';
 const stream = pretty({
   colorize: true,
   sync: true,
+  translateTime: 'HH:MM:ss',
+  ignore: 'pid,hostname',
 });
 
 const logger = pino(stream);
