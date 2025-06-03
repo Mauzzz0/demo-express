34ce8c53f71f55fe721deae2bc69a3051f9a47f7
Ruslan Semak <ruslan.semack1501@gmail.com>
2025-06-03 19:01:17 +0300

    feat: Validation improved

--- src/validation/types.ts
+++
@@ -1,3 +0,0 @@
-export type Constructor<T> = {
-  new (...args: unknown[]): T;
-};
--- src/validation/validate.ts
+++ src/validation/validate.ts
@@ -1,22 +1,19 @@
-import { plainToInstance } from 'class-transformer';
+import { ClassConstructor, plainToInstance } from 'class-transformer';
 import { validateSync } from 'class-validator';
 import { BadRequestException } from '../errors';
-import { Constructor } from './types';
 
-export const validate = <T extends object, V>(cls: Constructor<T>, data: V): T => {
+export const validate = <T extends object, V>(cls: ClassConstructor<T>, data: V): T => {
   const instance = plainToInstance<T, V>(cls, data);
   const errors = validateSync(instance);
 
   if (errors.length) {
-    const constraints = errors[0].constraints;
-    let message = 'Unknown validation error';
+    const [{ constraints }] = errors;
 
     if (constraints) {
-      const key = Object.keys(constraints)[0];
-      message = constraints[key];
+      throw new BadRequestException(constraints[Object.keys(constraints)[0]]);
     }
 
-    throw new BadRequestException(message);
+    throw new BadRequestException('Unknown validation error');
   }
 
   return instance;
